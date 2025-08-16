5c0b600c8641aa33abf3de810946bfe3793224e9
ekarvelas <ekarvelas@src.gr>
2025-08-16 16:24:29 +0300

    init

---
+++ src/main/java/com/hua/ss_it21114/SsIt21114Application.java
@@ -0,0 +1,13 @@
+package com.hua.ss_it21114;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsIt21114Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsIt21114Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hua/ss_it21114/SsIt21114ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hua.ss_it21114;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsIt21114ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
