c2a94d5e2ff63958b552675dc47a1ffcee171f8f
ekarvelas <ekarvelas@src.gr>
2025-08-25 16:42:05 +0300

    dev docker containers with auto reload on save

--- src/main/java/com/hua/ss_it21114/SsIt21114Application.java
+++ src/main/java/com/hua/ss_it21114/SsIt21114Application.java
@@ -2,6 +2,9 @@ package com.hua.ss_it21114;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SsIt21114Application {
@@ -10,4 +13,13 @@ public class SsIt21114Application {
         SpringApplication.run(SsIt21114Application.class, args);
     }
 
+    @RestController
+    @RequestMapping("/api/test")
+    public static class TestController {
+
+        @GetMapping
+        public String hello() {
+            return "Hello world!";
+        }
+    }
 }
